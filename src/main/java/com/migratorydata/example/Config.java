b05ae9f51d91a9f83b572bf4d30595c3f2413551
Laurentiu <laurentiu.luca@migratorydata.com>
2021-12-14 16:13:44 +0200

    minor updates

--- src/main/java/com/migratorydata/example/Config.java
+++ src/main/java/com/migratorydata/example/Config.java
@@ -6,7 +6,7 @@ public class Config {
 
     public static final String[] server = new String[] {"live.subjecthub.com"};
 
-    public static final String subject = "/s/bKql1r/test";
+    public static final String subject = "subject";
 
-    public static final String token = "s:bKql1r:TawtwO:s.p";
+    public static final String token = "key";
 }
