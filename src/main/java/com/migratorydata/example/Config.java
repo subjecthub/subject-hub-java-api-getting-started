1d5c39302a47e8137abe5b7a89386844b61ed03f
Laurentiu <laurentiu.luca@migratorydata.com>
2021-12-14 16:09:47 +0200

    first commit

---
+++ src/main/java/com/migratorydata/example/Config.java
@@ -0,0 +1,12 @@
+package com.migratorydata.example;
+
+public class Config {
+
+    public static final boolean encryption = true;
+
+    public static final String[] server = new String[] {"live.subjecthub.com"};
+
+    public static final String subject = "/s/bKql1r/test";
+
+    public static final String token = "s:bKql1r:TawtwO:s.p";
+}
